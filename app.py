4ed2330c0dd96ebdc3a01ad260c5f529fd471bbd
ergashbekova06-sys <ergashbekova06@bk.ru>
2025-11-26 22:16:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -75,9 +75,7 @@ if csv_text.strip():
 
             ax.bar([p - 0.2 for p in x], q, width=0.4, label="Качество знаний")
             ax.bar([p + 0.2 for p in x], u, width=0.4, label="Успеваемость")
-            ax.bar([p - 0.2 for p in x], q, width=0.4, label="Качество знаний")
-ax.bar([p + 0.2 for p in x], u, width=0.4, label="Успеваемость")
-
+            
 # ---- Добавляем проценты над столбиками ----
 for i, value in enumerate(q):
     ax.text(i - 0.2, value + 1, f"{value}%", ha='center')
