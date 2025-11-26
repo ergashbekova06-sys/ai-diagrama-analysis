8a4232eab61bbb9c0163ea43cbd7d0d47fb6fa15
ergashbekova06-sys <ergashbekova06@bk.ru>
2025-11-26 22:33:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if csv_text.strip():
         st.info(f"Колонка успеваемости: **{success_col}**")
 
         # Типы оценивания
-        assess_types = ["СОР 1", "СОР 2", "СОЧ"]
+        assess_types = ["БЖБ 1", "БЖБ 2", "ТЖБ"]
 
         # Для Word
         document = Document()
